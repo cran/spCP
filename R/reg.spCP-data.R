9681f42ab5f654854ea1afe59fb5a2c3614ce9fe
Samuel I. Berchuck <sib2@duke.edu>
2018-07-13 15:20:03 +0000

    version 1.0

---
+++ R/reg.spCP-data.R
@@ -0,0 +1,7 @@
+#' Example \code{spCP} object
+#'
+#' An example \code{spCP} object obtained from the example in the documentation for the
+#' \code{spCP} function.
+#'
+#' @usage data(reg.spCP)
+"reg.spCP"
