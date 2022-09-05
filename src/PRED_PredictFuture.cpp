b9444cc825cc52f3c9600a024e4cbe85a0a98cc5
Samuel I. Berchuck <sib2@duke.edu>
2022-09-05 05:20:02 +0000

    version 1.3

--- src/PRED_PredictFuture.cpp
+++ src/PRED_PredictFuture.cpp
@@ -30,7 +30,8 @@ arma::cube PredictFuture(Rcpp::List DatObj_List, Rcpp::List Para_List, int NKeep
 
   //Verbose output
   arma::vec VerboseSeq;
-  VerboseSeq << 0.25 << 0.50 << 0.75;
+  VerboseSeq = {0.25, 0.50, 0.75}; 	// new standard modern C++ brace initialization
+  // VerboseSeq << 0.25 << 0.50 << 0.75;
   VerboseSeq *= NKeep;
   Rcpp::Rcout << std::fixed << "Future Predictions: 0%.. ";
 
